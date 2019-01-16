778b5c4e1aab4451f67fe712f7954179bc4aa655
Roman Kostka <roman.kostka@sap.com>
2019-01-17 00:03:06 +0100

    initial setup

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-function enigma () {
-    var answer = document.forms["form"] ["useranswer"];
 
-    if (answer.value == "hello"){
-        window.alert("hello");
-    }
-}
+Vue.component('riddle', {
+    props: ['msg'],
+    template: '<div><div class="enigma-box"> {{ msg }} </div></div>'
+  })
+
+
+  var app = new Vue({
+    el: '#app',
+    data: {
+        message: [
+            { id: 0, msg: 'first image', correctAnswer:'Zmlyc3Q='},
+            { id: 1, msg: 'second image', correctAnswer:'c2Vjb25k'}
+        ],
+        counter: 0,
+        answer: 'type answer here...'
+    },
+    methods: {
+        submit: function (event) {
+            if (atob(app.message[app.counter].correctAnswer) == app.answer) {
+                app.counter += 1
+            }
+            else {
+                alert('wrong')
+            }
+        }
+      }
+  })
